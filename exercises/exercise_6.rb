0aae97cf6a78d8f1f2414308b557083b119541a5
Danek <danek.fill@outlook.com>
2022-08-22 20:36:32 +0000

    exercise 6 completed

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,15 +9,15 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
-store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
-store1.employees.create(first_name: "Jhurram", last_name: "Virani", hourly_rate: 60)
-store1.employees.create(first_name: "Mhurram", last_name: "Virani", hourly_rate: 60)
-store1.employees.create(first_name: "Ihurram", last_name: "Virani", hourly_rate: 60)
-store1.employees.create(first_name: "Shurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Jhurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Mhurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Ihurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Shurram", last_name: "Virani", hourly_rate: 60)
 
-store2.employees.create(first_name: "Burram", last_name: "Virani", hourly_rate: 60)
-store2.employees.create(first_name: "Curram", last_name: "Virani", hourly_rate: 60)
-store2.employees.create(first_name: "Uurram", last_name: "Virani", hourly_rate: 60)
-store2.employees.create(first_name: "Qurram", last_name: "Virani", hourly_rate: 60)
-store2.employees.create(first_name: "Iurram", last_name: "Virani", hourly_rate: 60)
-store2.employees.create(first_name: "Zurram", last_name: "Virani", hourly_rate: 60)
+@store2.employees.create(first_name: "Burram", last_name: "Virani", hourly_rate: 60)
+@store2.employees.create(first_name: "Curram", last_name: "Virani", hourly_rate: 60)
+@store2.employees.create(first_name: "Uurram", last_name: "Virani", hourly_rate: 60)
+@store2.employees.create(first_name: "Qurram", last_name: "Virani", hourly_rate: 60)
+@store2.employees.create(first_name: "Iurram", last_name: "Virani", hourly_rate: 60)
+@store2.employees.create(first_name: "Zurram", last_name: "Virani", hourly_rate: 60)
