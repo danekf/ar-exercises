47328e7f30a7c9bc07a5f7b6b32093ae13f04fc1
Danek <danek.fill@outlook.com>
2022-08-22 21:04:48 +0000

    validation added for store and employees

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,4 +1,9 @@
 class Employee < ActiveRecord::Base
   belongs_to :store
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, numericality: {greater_than: 40, less_than: 200, message: 'hourly rate must be between 40 and 200'}
+  
 end
+
