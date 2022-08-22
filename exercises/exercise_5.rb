6ddc5a52bb12eeb7dffa84d5e5e19d54101a3c61
Danek <danek.fill@outlook.com>
2022-08-22 20:33:07 +0000

    exercise 5 completed

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,9 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+@stores_sum = Store.sum(:annual_revenue)
+average_revenue = @stores_sum / Store.count
+puts average_revenue
+
+@profitable_stores = Store.where(annual_revenue: ..1000000).count
+puts @profitable_stores
